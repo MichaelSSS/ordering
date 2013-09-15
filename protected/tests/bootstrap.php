3a250d741228dacfe6d2c1ccb8fad979a357106c
MichaelSSS <mgrov@yandex.ru>
2013-09-15 19:51:36 +0300

    add login functionality

---
+++ protected/tests/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+
+// change the following paths if necessary
+$yiit=dirname(__FILE__).'/../../../yii/framework/yiit.php';
+$config=dirname(__FILE__).'/../config/test.php';
+
+require_once($yiit);
+require_once(dirname(__FILE__).'/WebTestCase.php');
+Yii::$classMap = array(
+            'PHPUnit_Extensions_Story_TestCase' => 'd:\mine\Web\yii\framework\test\PHPUnit\Extensions\Story\TestCase.php',
+        );
+Yii::createWebApplication($config);

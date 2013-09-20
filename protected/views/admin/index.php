27f76a9609ff5b605ebf937a0ede5b1ab548ceb6
MichaelSSS <mgrov@yandex.ru>
2013-09-20 18:41:01 +0300

    restore

--- protected/views/admin/index.php
+++ protected/views/admin/index.php
@@ -1,112 +1,48 @@
 <?php $this->renderPartial('/user/_del'); ?> <!--modal-->
 
-<?php $this->widget('bootstrap.widgets.TbTabs', array(
-         'type' => 'tabs',
-    'placement' => 'above', // 'above', 'right', 'below' or 'left'
-         'tabs' => array(
-        array('label' => 'Administration',
-            'content' => '<p>This page is appointed for creating new and managing existing users</p>',
-             'active' => true
-            ),
-        ),
-    ));
-?>
+<div id="wrapper">
+<h6>This page is appointed to create new and managing existing users</h6>
 
-<?php echo CHtml::link('Create New User', array('admin/create'));
+<?php
+    echo CHtml::link('Create New User',array('admin/create'));
+
     $dataProvider = $model->search();
-    //file_put_contents('d:\\log.txt', print_r($dataProvider->getTotalItemCount(),true));
+
     echo '<div id="search-result">Number of Found Users <span id="search-result-count">'
-        . $dataProvider->getTotalItemCount() . '</span></div>';
+                 . $dataProvider->getTotalItemCount() . '</span></div>';
+
 ?>
 
-<?php /** @var BootActiveForm $form */
-    $form = $this->beginWidget('bootstrap.widgets.TbActiveForm', array(
-                            'id' => 'search-form',
-        'enableClientValidation' => true,
-                 'clientOptions' => array(
-              'validateOnSubmit' => true,
+<div class="form">
+
+    <?php $form=$this->beginWidget('CActiveForm', array(
+    	'id'=>'search-form',
+    	'enableClientValidation'=>true,
+    	'clientOptions'=>array(
+    		'validateOnSubmit'=>true,
         ),
-               'type' => 'inline',
-        'htmlOptions' => array(
-              'class' => '',
-            )
     ));
-?>
-<div class="row">
-    <div class="span10">
-        <fieldset>
-            <legend>Search by</legend>
-            <p>Field Filter</p>
-            <div class="control-group">
-                <div class="controls">
-                    <div class="span3">
-                        <?php echo $form->dropDownListRow($fields, 'keyField',
-                            array('All Columns', 'User Name', 'First Name', 'Last Name', 'Role'),
-                            array('class' =>' span3', 'options' => array(
-                                array_search('User Name', $fields->keyFields) => array('selected' => true)
-                            ))
-                        );
-                        ?>
-                    </div>
-                    <div class="span3">
-                        <?php echo $form->dropDownListRow($fields, 'criteria',
-                            array('equals', 'not equal to', 'starts with', 'contains', 'does not contain'),
-                            array('class' => 'span3', 'options' => array(
-                                array_search('starts with', $fields->criterias) => array('selected' => true)
-                            ))
-                        );
-                        ?>
-                    </div>
-                    <div class="span3">
-                        <?php echo $form->textField($fields, 'keyValue', array('class' => 'span3', 'placeholder' => 'Search')); ?>
-                        <div class="row">
-                            <input class='btn btn-info pull-right' type='submit' value='Search'>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </fieldset>
-    </div>
+    ?>
+    <fieldset><legend>&nbspSearch by&nbsp</legend>
+        <div>Field Filter</div>
+        <div id="search-fields">
+        <?php
+            echo $form->dropDownlist($fields,'keyField',$fields->keyFields, array(
+                'options'=>array(
+                    array_search('User Name',$fields->keyFields) => array('selected'=>true),
+                )
+            ));
+            echo $form->dropDownlist($fields,'criteria',$fields->criterias, array(
+                'options'=>array(
+                    array_search('starts with',$fields->criterias) => array('selected'=>true),
+                 )
+            ));
+            echo $form->textField($fields,'keyValue');
+        ?>
+        </div>
+        <input class='btn' type='submit' value='Search'>
+    </fieldset>
+    <?php $this->endWidget(); ?>
 </div>
-<?php $this->endWidget(); ?>
-
 <?php $this->renderPartial('grid',array('dataProvider'=>$dataProvider, 'fields'=>$fields)); ?>
-
-
-<!-- <div class="form">
-
-    <?php /*$form=$this->beginWidget('CActiveForm', array(
-        'id'=>'search-form',
-        'enableClientValidation'=>true,
-        'clientOptions'=>array(
-            'validateOnSubmit'=>true,
-        ),
-    ));
-    */?>
-        <fieldset>
-            <legend>Search by</legend>
-            <p>Field Filter</p>
-            <div id="search-fields">
-                <?php
-/*                        echo $form->dropDownlist($fields,'keyField',$fields->keyFields, array(
-                        'options'=>array(
-                            array_search('User Name',$fields->keyFields) => array('selected'=>true)
-                        )
-                    ));
-                    echo $form->dropDownlist($fields,'criteria',$fields->criterias,
-                        array(
-                            'options'=>array(
-                                array_search('starts with',$fields->criterias) => array('selected'=>true
-                             ))
-                    ));*/?>
-                <div class="">
-                   <?/*= $form->textField($fields,'keyValue');*/?>
-                </div>
-
-
-            </div>
-            <input class='btn' type='submit' value='Search'>
-        </fieldset>
-    <?php /*$this->endWidget(); */?>
-</div>-->
-
+</div>

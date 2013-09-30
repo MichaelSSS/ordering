9caf0ccdeefd59b0018b42cf36626e38cf5bd5d0
Dmitriy <dmitriy.dreamcast@gmail.com>
2013-09-30 17:40:45 +0300

    adminui & errorui

--- protected/views/site/error.php
+++ protected/views/site/error.php
@@ -14,7 +14,7 @@
         ?>
         <div class="err-message">
             <div class="text-error">
-                <h2 class=''>Error <?php echo $code ?></h2>
+                <h2 class='label label-warning'>Error <?php echo $code ?></h2>
                 <?php echo CHtml::encode($message);
                 if ( $code == 403 ) {
                     echo '<br>Please ' . CHtml::link('login',array('site/login')) . ' under appropriate User Name';
@@ -24,8 +24,7 @@
         </div>
         <div class="row">
             <div class="span6">
-                <p class='lead'>Посмотрите в наши честные глаза, развве могло что-то "сломаться" в приложении ?
-                    Напишите нам в Skype:</p>
+                <p class='lead'>Развве могло что-то "сломаться" в приложении ? Вы можете написать нам в Skype:</p>
                 <ul class='well text-center'>
                     <li>m_i_c_h_a_el777</li>
                     <li>dima_zhdan</li>

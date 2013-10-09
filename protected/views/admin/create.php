f42d5a091567ce43a6fc9ce3c83825d5a3675268
sekret47 <sekret47@gmail.com>
2013-10-09 11:54:21 +0300

    add password generator

--- protected/views/admin/create.php
+++ protected/views/admin/create.php
@@ -26,8 +26,9 @@
 
         <?php echo $form->passwordFieldRow($model, 'confirmPassword'); ?>
 
-       <div><button class="show_pass">Show password</button>
-           <button class="generate_pass">Generate password</button>
+       <div class="controls password_buttons">
+           <input type="button" class="show_pass btn-info btn-mini" value="Show/Hide password"/>
+           <input type="button" class="generate_pass btn-info btn-mini" value="Generate "/>
        </div>
         <?php echo $form->textFieldRow($model, 'email') ?>
         
@@ -99,56 +100,4 @@
             <?php $this->widget('bootstrap.widgets.TbButton', array('buttonType'=>'reset', 'label'=>'Refresh')); ?>
         </div>
 <?php $this->endWidget(); ?>
-
-<script>
-    $(document).ready(function () {
-        $('#User_password').tooltip();
-
-        $('.show_pass').click(function(){
-            element =  $('#User_password');
-            element.replaceWith(element.clone().attr('type',(element.attr('type') == 'password') ? 'text' : 'password'))
-        })
-        $('.show_pass').on('click',(function(){
-            element =  $('#User_confirmPassword');
-            element.replaceWith(element.clone().attr('type',(element.attr('type') == 'password') ? 'text' : 'password'))
-            return false;
-        }))
-        $('.generate_pass').click(function(){
-            var pass = '';
-            var lowLetter = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-            var highLetter = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-            var number = [1,2,3,4,5,6,7,8,9,0];
-            var spChar = ['!','@','#','$','%','^','&','*','(',')','_','-','+','=','~','/',';',':','<','>','?'];
-
-            for(i=0;i<2;i++){
-                pass += lowLetter[parseInt(Math.random()*lowLetter.length)]
-                pass += highLetter[parseInt(Math.random()*highLetter.length)]
-                pass += number[parseInt(Math.random()*number.length)]
-                pass += spChar[parseInt(Math.random()*spChar.length)]
-            }
-
-            pass = pass.split('')
-
-             function shuffle( b )
-            {
-                var i = this.length, j, t;
-                while( i )
-                {
-                    j = Math.floor( ( i-- ) * Math.random() );
-                    t = b && typeof this[i].shuffle!=='undefined' ? this[i].shuffle() : this[i];
-                    this[i] = this[j];
-                    this[j] = t;
-                }
-
-                return this;
-            };
-            shuffle(pass);
-            pass = pass.join('');
-            console.log(pass);
-            $('#User_password').val(pass);
-            $('#User_confirmPassword').val(pass);
-            return false;
-        })
-
-    });
-</script>
+<?php $this->renderPartial('_password');?>

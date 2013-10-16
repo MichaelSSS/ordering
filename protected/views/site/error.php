dcfadc5ef81a2f6c31db06ee44e502c6a21b5f03
dmitiy <dmitrysev@ua.fm>
2013-10-16 17:37:08 +0300

    error UI

--- protected/views/site/error.php
+++ protected/views/site/error.php
@@ -8,41 +8,48 @@
 ); ?>
 
 <div class="wrp1">
-    <div class='panel panel-warning'>
-        <div class='panel-heading'>
-            <p class='lead text'><?php echo CHtml::encode($message);?></p>
-        </div>
-        <div class='panel-body'>
-            <div class="span4 hero-unit">
-                <h1 class='btn'>
-                    <?= $code; ?>
-                    <?php if ( $code == 404 ) {
-                        echo '<p class="lead">Page not found.</p>';
+    <div class="span8 offset2">
+        <div class="row">
+            <div class='panel panel-warning'>
+                <div class='panel-heading'>
+                    <p class='lead text'><?php echo CHtml::encode($message);?></p>
+                </div>
+                <div class='panel-body'>
+                    <div class="span3">
+                        <h1 class='text-center oval'>
+                            <?= $code; 
+                                if( $code==403 ) {
+                                    echo '<p class="lead">Error</p>';
+                                }
+                                if ( $code == 404 ) {
+                                echo '<p class="lead">Page not found</p>';
+                                }
+                                if ( $code == 500 ) {
+                                    echo '<p class="lead">Ooops, that\'s an error</p>';
+                                }
+                            ?>
+                        </h1>   
+                    </div>                
+                    <div class='span4 pull-right'>
+                        <img class='img-circle' title='A team of developers of LAMP(PHP)' src="<?php echo Yii::app()->getBaseUrl(true) . '/images/LAMP.jpg' ?>" />
+                    </div>  
+                </div>
+                <div class='panel-footer'>
+                    <?php if ( $code == 403 ) {
+                            echo '<p><i class="icon-info-sign icon-large"></i> Please '  . CHtml::link('login', array('site/login')) .
+                            ' under appropriate User Name</p>';
+                        } if ( $code == 404 )
+                            echo '<p><i class="icon-info-sign icon-large"></i> <i>Sorry, this page is not available.</i></p>';
+                        if ( $code == 500 ) {
+                            echo '<p><i class="icon-info-sign icon-large"></i> <i>Internal Server Error</i></p>';
                         }
                     ?>
-                </h1>
-                <ul class='nav'>
-                    <li class='btn btn-danger'>E</li>
-                    <li class='btn btn-danger'>R</li>
-                    <li class='btn btn-danger'>R</li>
-                    <li class='btn btn-danger'>O</li>
-                    <li class='btn btn-danger'>R</li>
-                </ul>
-            </div>
-            <div class="span5">
-                <div class='span5'>
-                    <img class='img-circle' src="<?php echo Yii::app()->getBaseUrl(true) . '/images/LAMP.jpg' ?>" />
                 </div>
             </div>
         </div>
-        <div class='panel-footer'>
-            <?php if ( $code == 403 ) {
-                echo '<p class="lead">Please '  . CHtml::link('login', array('site/login')) .
-                    ' under appropriate User Name</p>';
-                }
-            ?>
-        </div>
+      
     </div>
+  
 </div>
 
     
